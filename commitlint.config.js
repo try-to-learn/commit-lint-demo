b9a690649e2bc913950e0e4001b7ee059eca4c88
sibi.a <sibi.a@everi.com>
2021-03-07 12:47:49 +0530

    chore: commit lint config file added

---
+++ commitlint.config.js
@@ -0,0 +1,44 @@
+// Rules are made up by a name and a configuration array. The configuration array contains:
+
+// Level [0..2]: 0 disables the rule. For 1 it will be considered a warning for 2 an error.
+// Applicable always|never: never inverts the rule.
+// Value: value to use for this rule.
+
+module.exports = {
+	parserPreset: 'conventional-changelog-conventionalcommits',
+    formatter: '@commitlint/format',
+	rules: {
+		'body-leading-blank': [1, 'always'],
+		'body-max-line-length': [2, 'always', 100],
+		'footer-leading-blank': [1, 'always'],
+		'footer-max-line-length': [2, 'always', 100],
+		'header-max-length': [2, 'always', 100],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-case': [
+			0,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+			],
+		],
+	},
+};
